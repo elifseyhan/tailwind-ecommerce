5811636c40f8ab7e32bdc38537cef1a6976d498d
Elif Seyhan <elifseyhan@Elif-MacBook-Air.local>
2022-04-17 00:31:33 +0300

    Container and screens are customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
     extend: {
       fontFamily:{
         poppins: 'Poppins, sans-serif',
